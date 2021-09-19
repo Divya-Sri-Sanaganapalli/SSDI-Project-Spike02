9fad400c0664870aaa3a5ad6ddcedeeaf0fe6414
Divya-Sri-Sanaganapalli <dsanagan@uncc.edu>
2021-09-19 16:10:53 -0400

    Connecting mongodb

---
+++ appdemo.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const {MongoClient} = require("mongodb");
+
+const url = 'mongodb://localhost:27017/';
+const databasename = "SSDI-Sprint2";
+
+app.get('/', async(req,res) => {
+    MongoClient.connect(url ,{ useNewUrlParser: true })
+        .then((client) => {
+            const connect = client.db(databasename);
+            // Connect to collection
+            const collection = connect.collection("DemoDB");
+        
+            collection.find({}).toArray().then((ans) => {
+            res.send(ans);
+            });
+    }).catch((err) => {
+        // Printing the error message
+        res.send(err.Message);
+    })
+});
+
+app.listen(port, () => {
+    console.log('The server is running on port', port);
+});
